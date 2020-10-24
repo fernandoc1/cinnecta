920f91327868269bc8ee18658b95c539d414ba5c
Fernando Carvalho <fernandocarvalhocoelho@gmail.com>
2020-10-24 17:14:53 -0300

    Added code to be uploaded to Google Cloud.

--- backend/receive_text.py
+++ backend/receive_text.py
@@ -1,14 +1,6 @@
 import flask
 
-def hello_world(request):
-    """Responds to any HTTP request.
-    Args:
-        request (flask.Request): HTTP request object.
-    Returns:
-        The response text or any set of values that can be turned into a
-        Response object using
-        `make_response <http://flask.pocoo.org/docs/1.0/api/#flask.Flask.make_response>`.
-    """
+def receive_text(request):
     response = flask.Response('{"data": "ok"}')
     response.headers['Access-Control-Allow-Origin'] = '*'
     response.headers['Access-Control-Allow-Methods'] = "GET,HEAD,OPTIONS,POST,PUT"
