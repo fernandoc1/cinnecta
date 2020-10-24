08827c35bdeefbaca3fb57e043b71ea6358573b8
Fernando Carvalho <fernandocarvalhocoelho@gmail.com>
2020-10-24 17:14:21 -0300

    Added code to test request.

---
+++ backend/app.py
@@ -0,0 +1,16 @@
+#! /usr/bin/python3
+
+import flask
+import pprint
+
+#Code that is going to Google Cloud
+import receive_text
+
+app = flask.Flask(__name__, static_url_path='', static_folder='../html/')
+
+@app.route('/receive_text')
+def receiveText():
+    print(flask.request)
+    return receive_text.receive_text(flask.request)
+
+app.run(debug=True, port=5000) #run app in debug mode on port 5000
