61d0a80414e985e2a6fa8ac548822026dc509143
Fernando Carvalho <fernandocarvalhocoelho@gmail.com>
2020-10-25 10:54:36 -0300

    [Improvement] Code to retrieve full dictionary.

--- backend/get_data.py
+++ backend/get_data.py
@@ -2,11 +2,23 @@
 
 import db_connector
 
+def getTextCollection():
+    db = db_connector.getDBConnection()
+    return db["text_collection"].find()
+
+def getFullDict():
+    textCollection = getTextCollection()
+    fullDict = []
+    for document in textCollection:
+        for entry in document["text_dict"]:
+            fullDict.append(entry)
+    return fullDict
+
 def getData():
     db = db_connector.getDBConnection()
     cursor = db["text_collection"].find()
     for document in cursor:
-        print(document)
+        print(document["title"])
 
-getData()
+print(getFullDict())
 
