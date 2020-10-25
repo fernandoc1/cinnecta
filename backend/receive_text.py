beba97fd12a9c9696eed379945093cccffd5f0b7
Fernando Carvalho <fernandocarvalhocoelho@gmail.com>
2020-10-25 09:28:43 -0300

    Initial count of tokens.

--- backend/receive_text.py
+++ backend/receive_text.py
@@ -1,9 +1,20 @@
 import flask
 
 def receive_text(request):
-    print(request.get_json())
+    jsonDict = request.get_json()
+    text = jsonDict['text']
 
-    response = flask.Response('{"data": "ok"}')
+    textDict = dict()
+    tokens = text.split()
+    for tok in tokens:
+        if tok in textDict:
+            textDict[tok] += 1
+        else:
+            textDict[tok] = 1
+
+    #print(textDict)
+
+    response = flask.Response('{"response": "ok"}')
     response.headers['Access-Control-Allow-Origin'] = '*'
     response.headers['Access-Control-Allow-Methods'] = "GET,HEAD,OPTIONS,POST,PUT"
     response.headers['Access-Control-Allow-Headers'] = "Origin, X-Requested-With, Content-Type, Accept, Authorization";
