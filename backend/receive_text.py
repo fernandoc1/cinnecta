0fcf88e8ee882f761aaa88186d532526bdd361ac
Fernando Carvalho <fernandocarvalhocoelho@gmail.com>
2020-10-25 09:09:28 -0300

    Initial test receiving data.

--- backend/receive_text.py
+++ backend/receive_text.py
@@ -1,6 +1,8 @@
 import flask
 
 def receive_text(request):
+    print(request.get_json())
+
     response = flask.Response('{"data": "ok"}')
     response.headers['Access-Control-Allow-Origin'] = '*'
     response.headers['Access-Control-Allow-Methods'] = "GET,HEAD,OPTIONS,POST,PUT"
