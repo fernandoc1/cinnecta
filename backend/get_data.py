def4f0d39718479ba14d7043805ba1fd1b28ee86
Fernando Carvalho <fernandocarvalhocoelho@gmail.com>
2020-10-25 17:03:56 -0300

    [Bugfix] Fixed bug while creating N vector.

--- backend/get_data.py
+++ backend/get_data.py
@@ -23,14 +23,27 @@ def getFullDict(dictEntry):
 
 def getNDict(dictEntry):
     textCollection = getTextCollection()
-    documentDict = dict()
+    fullDict = dict()
     for document in textCollection:
+        currentDocument = document[dictEntry]
+        for entry in currentDocument:
+            if entry in fullDict:
+                fullDict[entry] += currentDocument[entry]
+            else:
+                fullDict[entry] = currentDocument[entry]
+    documentDict = dict()
+    textCollection = getTextCollection()
+    for document in textCollection:
+        print(document)
         currentDocument = document[dictEntry]
         wordList = []
         amountList = []
-        for entry in currentDocument:
+        for entry in fullDict:
             wordList.append(entry)
-            amountList.append(currentDocument[entry])
+            if entry in currentDocument:
+                amountList.append(currentDocument[entry])
+            else:
+                amountList.append(0)
         documentDict[document["title"]] = [wordList, amountList]
     return documentDict 
 
